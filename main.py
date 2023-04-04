6331f5a6d5364033a9916e0ef41557ac13a6730a
SX-7 <sn1.test.preria.2002@gmail.com>
2023-04-04 18:49:21 +0200

    Moved to env vars

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from kafka import KafkaConsumer
+import msgpack
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+server_ip = os.getenv("SERVER_IP")
 
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer('my-topic',
                          bootstrap_servers=[':9092'],
                          request_timeout_ms=10000,
                          session_timeout_ms=5000,
-                         security_protocol="",
-                         sasl_mechanism="",
-                         sasl_plain_username="",
-                         sasl_plain_password="")
+                         value_deserializer=msgpack.unpackb
+                         )
+print(consumer.bootstrap_connected())
+for message in consumer:
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
 
-print(consumer.metrics())
