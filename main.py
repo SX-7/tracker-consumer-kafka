46cab55e23779e99a7b14cbcb17ae42eb3c7816c
SX-7 <sn1.test.preria.2002@gmail.com>
2023-05-06 17:22:40 +0200

    Added basic auth

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ from kafka import KafkaConsumer
 import msgpack
 from dotenv import load_dotenv
 import os
+from pytermgui import tim
 
 load_dotenv()
 
-server_ip = os.getenv("SERVER_IP")
+server_ip = os.getenv("KAFKA_SERVER_IP")
+kafka_username=os.getenv("KAFKA_USERNAME")
+kafka_password=os.getenv("KAFKA_PASSWORD")
 
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('my-topic',
-                         bootstrap_servers=[str(server_ip+':9092')],
+consumer = KafkaConsumer(f'discord-{kafka_username}-all',
+                         bootstrap_servers=[str(server_ip)],
                          request_timeout_ms=10000,
                          session_timeout_ms=5000,
                          value_deserializer=msgpack.unpackb,
-                         key_deserializer=msgpack.unpackb
+                         key_deserializer=msgpack.unpackb,
+                         sasl_mechanism="PLAIN",
+                         security_protocol="SASL_PLAINTEXT",
+                         sasl_plain_username=kafka_username,
+                         sasl_plain_password=kafka_password,
+                         auto_offset_reset="earliest"
                          )
-print(consumer.bootstrap_connected())
+
+if consumer.bootstrap_connected():
+    tim.print("[blue]INIT[/]    Estabilished connection with Kafka server")
 
 try:
     for message in consumer:
-        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                             message.offset, message.key,
-                                             message.value))
+        tim.print(f'Topic {message.topic}, partition {message.partition}, offset {message.offset}, key {message.key}, action {message.value["action_id"]}')
 except KeyboardInterrupt:
     consumer.close
